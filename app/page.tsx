ccba4b9d834bee385beebdca73115a51af7a047c
joechinjiahao-art <joechinjiahao@gmail.com>
2026-03-25 13:20:00 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import { supabase } from "../../lib/supabase";
+import { supabase } from "@/lib/supabase";
 import { v4 as uuidv4 } from "uuid";
 
 export default function Home() {
